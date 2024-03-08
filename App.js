1d0928626ea101a1d85ed89d1999fdc223471789
genova <genovadavid44@gmail.com>
2024-03-08 18:00:44 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const fecthUsers = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/api/?nat=fr');
+      const result = await response.json()
+      console.log(result)
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fecthUsers();
+  }, 
+  [])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Illustration des requêtes API en React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
